099ce6449c3df4053d6cc01144b5ad6daca9c49e
Marcelo de Lima Gomes <marcelo.lima@unifenas.br>
2023-04-12 08:15:07 -0300

    crg

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -167,7 +167,7 @@ export function Home() {
           })}
         </MinesContent>
 
-        <AccessGame href="https://www.pixluck.bet/register?id=63fe292e91c096002450f877" target="_blank">
+        <AccessGame href="https://www.pixluck.bet/register?id=63895c485f959a0027978253" target="_blank">
           <img src={link} /> Acessar Game
         </AccessGame>
 
