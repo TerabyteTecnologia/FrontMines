a5e355d7e598b05f6365c9a57d36d5d9c8d69f95
unknown <marcelo.lima.gomes.23@gmail.com>
2023-04-17 20:01:41 -0300

    clz

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -174,7 +174,7 @@ export function Home() {
           })}
         </MinesContent>
 
-        <AccessGame href="https://afiliado.realsbet.com/visit/?bta=41156&brand=realsbet"  target="_blank">
+        <AccessGame href="https://www.pixluck.bet/register?id=63fe292e91c096002450f877"  target="_blank">
           <img src={link} /> Acessar Game
         </AccessGame>
 
