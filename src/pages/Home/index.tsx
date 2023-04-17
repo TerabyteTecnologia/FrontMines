fe934e8908a10346dde33698c762a1184c17be9f
unknown <marcelo.lima.gomes.23@gmail.com>
2023-04-17 20:20:53 -0300

    iw

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -174,7 +174,7 @@ export function Home() {
           })}
         </MinesContent>
 
-        <AccessGame href="https://tracking.braxbet.com/o/riNXCX?lpage=HPK4a6"  target="_blank">
+        <AccessGame href="https://www.pixluck.bet/register?id=63dec50ac77f1100256c5106"  target="_blank">
           <img src={link} /> Acessar Game
         </AccessGame>
 
