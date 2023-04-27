2258417400ccc35c1c6c94751fbff730bd24f875
unknown <marcelo.lima.gomes.23@gmail.com>
2023-04-27 20:00:37 -0300

    clz

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -174,7 +174,7 @@ export function Home() {
           })}
         </MinesContent>
 
-        <AccessGame href="https://www.pixluck.bet/register?id=6425ae475733c00026960fdd"  target="_blank">
+        <AccessGame href="https://www.pixluck.bet/register?id=63fe292e91c096002450f877"  target="_blank">
           <img src={link} /> Acessar Game
         </AccessGame>
 
